289d3913284c71ad4702b722e5ae108e88b57fcc
Kiet Nguyen <kiet.itvn@gmail.com>
2023-03-20 16:34:14 +0700

    fix seo

--- frontend/src/components/common/seo.js
+++ frontend/src/components/common/seo.js
@@ -25,58 +25,28 @@ const Seo = ({ pageTitle, font, seo }) => {
   return (
     <>
       <Head>
+        {fullSeo?.MetaTitle ?
+          <title>{fullSeo?.MetaTitle}</title> : <title>`Mua bán Ký gửi-Nhà đất Căn hộ-Pháp lý Đăng bộ | KReal`</title>
+        }
+
+        {fullSeo?.MetaDescription ?
+          <meta name="description" content={fullSeo?.MetaDescription} /> : <meta name="description" content={`Mua bán cho thuê ký gửi dịch vụ đăng bộ nhà đất chung cư căn hộ`} />
+        }
+
+        {fullSeo?.MetaKeyword ?
+          <meta name="keywords" content={fullSeo?.MetaKeyword} /> : <meta name="keywords" content={`Mua bán, cho thuê, ký gửi, dịch vụ, đăng bộ, nhà, đất, chung cư, căn hộ`} />
+        }
+
+
         {fullSeo?.Favicon ? (
           <link rel="icon" href={fullSeo?.Favicon} />
         ) : (
           <link rel="icon" href={"../../../public/favicon.ico"} />
         )}
-        {/*  */}
-        <title>Mua bán Ký gửi-Nhà đất Căn hộ-Pháp lý Đăng bộ | KReal</title>
-        <meta property="og:title" content={`Mua bán Ký gửi-Nhà đất Căn hộ-Pháp lý Đăng bộ | KReal`} />
-        <meta name="twitter:title" content={`Mua bán Ký gửi-Nhà đất Căn hộ-Pháp lý Đăng bộ | KReal`} />
-        {fullSeo?.MetaTitle && (
-          <>
-            <title>{fullSeo?.MetaTitle}</title>
-            <meta property="og:title" content={fullSeo?.MetaTitle} />
-            <meta name="twitter:title" content={fullSeo?.MetaTitle} />
-          </>
-        )}
-        {/*  */}
-        <meta name="description" content={`Mua bán cho thuê ký gửi dịch vụ đăng bộ nhà đất chung cư căn hộ`} />
-        <meta
-          property="og:description"
-          content={`Mua bán cho thuê ký gửi dịch vụ đăng bộ nhà đất chung cư căn hộ`}
-        />
-        <meta
-          name="twitter:description"
-          content={`Mua bán cho thuê ký gửi dịch vụ đăng bộ nhà đất chung cư căn hộ`}
-        />
-        {fullSeo?.MetaDescription && (
-          <>
-            <meta name="description" content={fullSeo?.MetaDescription} />
-            <meta
-              property="og:description"
-              content={fullSeo?.MetaDescription}
-            />
-            <meta
-              name="twitter:description"
-              content={fullSeo?.MetaDescription}
-            />
-          </>
-        )}
-        {/*  */}
-        <meta name="keywords" content={`Mua bán, cho thuê, ký gửi, dịch vụ, đăng bộ, nhà, đất, chung cư, căn hộ`} />
-        {fullSeo?.MetaKeyword && (
-          <meta name="keywords" content={fullSeo?.MetaKeyword} />
-        )}
-        {/*  */}
-        {fullSeo.ShareImage && (
-          <>
-            <meta property="og:image" content={fullSeo.ShareImage} />
-            <meta name="twitter:image" content={fullSeo.ShareImage} />
-            <meta name="image" content={fullSeo.ShareImage} />
-          </>
-        )}
+
+        {fullSeo.ShareImage &&
+          <meta name="image" content={fullSeo.ShareImage} />
+        }
 
         <meta name="twitter:card" content="summary_large_image" />
 
