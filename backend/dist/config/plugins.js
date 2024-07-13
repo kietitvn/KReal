ceb5e04bd2ff1e4af67cc2c8260eebc76605df7f
Kiet Nguyen <kiet.itvn@gmail.com>
2024-07-14 00:09:09 +0700

    test no strapi cloud

--- backend/dist/config/plugins.js
+++ backend/dist/config/plugins.js
@@ -1,3 +1,12 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => ({});
+module.exports = ({ env }) => ({
+    "vercel-deploy": {
+        enabled: true,
+        config: {
+            deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
+            apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
+            appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+            teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_TEAM_FILTER,
+            roles: ["strapi-super-admin"],
+        },
+    },
+});
