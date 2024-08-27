ea2c4b8171adef4726cfb9f0cd9bad78212cefa8
Kiet Nguyen <kiet.itvn@gmail.com>
2024-08-27 21:01:59 +0700

    update strapi 4135

--- backend/dist/config/plugins.js
+++ backend/dist/config/plugins.js
@@ -1,21 +1,8 @@
-module.exports = ({ env }) => ({
-    "users-permissions": {
-        config: {
-            jwtSecret: env("JWT_SECRET"),
-        },
-    },
-    upload: {
-        config: {
-            provider: "cloudinary",
-            providerOptions: {
-                cloud_name: env("CLOUDINARY_NAME"),
-                api_key: env("CLOUDINARY_KEY"),
-                api_secret: env("CLOUDINARY_SECRET"),
-            },
-            actionOptions: {
-                upload: {},
-                delete: {},
-            },
-        },
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// ./config/plugins.ts
+exports.default = () => ({
+    "media-prefix": {
+        enabled: true,
     },
 });
