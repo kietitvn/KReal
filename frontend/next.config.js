9d61c05f53a33bf43c65adc976b3f7865f74c846
kietitvn <kiet.itvn@gmail.com>
2025-05-22 17:05:46 +0700

    fix images

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,11 @@ const isProd = process.env.NODE_ENV === "production";
 const prefixURL = isProd ? "https://" : "http://";
 const baseUrl = isProd ? "batdongsan-hcm.ddns.net" : "localhost:1337";
 
+// Set NODE_TLS_REJECT_UNAUTHORIZED for development only
+if (!isProd) {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+}
+
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: false,
@@ -11,9 +16,30 @@ const nextConfig = {
     // storePicturesInWEBP: true,
   },
   images: {
-    domains: [baseUrl, 'batdongsan678.com', "localhost"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: baseUrl,
+        port: '',
+        pathname: '/**',
+      },  
+      {
+        protocol: 'https',
+        hostname: 'batdongsan678.com',
+        port: '',
+        pathname: '/**',
+      },  
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+        pathname: '/**',
+      },
+    ],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    dangerouslyAllowSVG: true,
+    minimumCacheTTL: 60,
   },
 };
 
