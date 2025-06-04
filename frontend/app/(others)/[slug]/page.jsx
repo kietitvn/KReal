282395a182a076c211d9f15a4faed181e0309c7a
kietitvn <kiet.itvn@gmail.com>
2025-06-04 16:03:30 +0700

    /frontend fix seo

--- frontend/app/(others)/[slug]/page.jsx
+++ frontend/app/(others)/[slug]/page.jsx
@@ -2,6 +2,9 @@ import ListingDetails from "@/components/details";
 import dynamic from "next/dynamic";
 
 export async function generateMetadata({ params, searchParams }) {
+  console.log('params',params);
+  console.log('searchParams',searchParams);
+
   const id = searchParams.id;
   const res = await fetch(process.env.baseUrl + "/api/products/" + id + "?&populate=*", {
     cache: 'no-store', // nếu bạn muốn luôn fetch mới
@@ -10,15 +13,19 @@ export async function generateMetadata({ params, searchParams }) {
   const data = response?.data;
   const slug = data?.attributes?.slug;
   const seoContent = data?.attributes?.seo;
+  console.log('seoContent',data);
   const title = seoContent?.MetaTitle ? seoContent?.MetaTitle : slug;
+  const description = seoContent?.MetaDescription ? seoContent?.MetaDescription : slug;
+  const keywords = seoContent?.MetaKeywords ? seoContent?.MetaKeywords : slug;
+  const images = data?.attributes?.cover?.data?.attributes?.url ;
   return {
     title,
-    description: seoContent?.MetaDescription,
-    keywords: seoContent?.MetaKeywords,
+    description,
+    keywords,
     openGraph: {
       title,
-      description: seoContent?.MetaDescription,
-      images: [seoContent?.ShareImage],
+      description,
+      images
     },
   };
 }
--- frontend/components/listing-grid/grid-v1/FeaturedItem.jsx
+++ frontend/components/listing-grid/grid-v1/FeaturedItem.jsx
@@ -1,9 +1,9 @@
 'use client';
+import Image from "next/image";
 import Link from "next/link";
 import { useSelector } from "react-redux";
 import { selectProducts } from "../../../features/products/productsSlice";
 import { doctien } from "../../../utils/currency";
-import Image from "next/image";
 
 const FeaturedItem = () => {
   const productData = useSelector(selectProducts);
@@ -20,102 +20,103 @@ const FeaturedItem = () => {
           } `}
         key={item?.id}
       >
-        <div
-          className={`feat_property home7 style4 ${isGridOrList ? "d-flex align-items-center" : undefined
-            }`}
-        >
-          <div className="thumb" style={{ position: 'relative', width: 342, height: 220 }}>
-            {imageUrl && imageUrl.length > 0 ? (
-              <Image
-                className="img-whp w-100 h-100 cover"
-                src={imageUrl[0]}
-                // width={342}
-                // height={220}
-                fill
-                style={{ objectFit: 'cover' }}
-              />
-            ) : (
-              <Image
-                className="img-whp w-100 h-100 cover"
-                src={
-                  item?.attributes?.cover?.data?.attributes?.formats?.small
-                    ?.url
-                }
-                alt={
-                  item?.attributes?.cover?.data?.attributes?.alternativeText
-                }
-                // width={342}
-                // height={220}
-                fill
-                style={{ objectFit: 'cover' }}
-              />
-            )}
-            <div className="thmb_cntnt">
-              <ul className="tag mb0">
-                {item?.attributes?.feature_ids?.data?.length > 0 && (
+        <Link href={`/${item?.attributes?.slug}?id=${item?.id}`}>
+          <div
+            className={`feat_property home7 style4 ${isGridOrList ? "d-flex align-items-center" : undefined
+              }`}
+          >
+            <div className="thumb" style={{ position: 'relative', width: 342, height: 220 }}>
+              {imageUrl && imageUrl.length > 0 ? (
+                <Image
+                  className="img-whp w-100 h-100 cover"
+                  src={imageUrl[0]}
+                  // width={342}
+                  // height={220}
+                  fill
+                  style={{ objectFit: 'cover' }}
+                />
+              ) : (
+                <Image
+                  className="img-whp w-100 h-100 cover"
+                  src={
+                    item?.attributes?.cover?.data?.attributes?.formats?.small
+                      ?.url
+                  }
+                  alt={
+                    item?.attributes?.cover?.data?.attributes?.alternativeText
+                  }
+                  // width={342}
+                  // height={220}
+                  fill
+                  style={{ objectFit: 'cover' }}
+                />
+              )}
+              <div className="thmb_cntnt">
+                <ul className="tag mb0">
+                  {item?.attributes?.feature_ids?.data?.length > 0 && (
+                    <li className="list-inline-item">
+                      <a href="#">Tiện ích</a>
+                    </li>
+                  )}
                   <li className="list-inline-item">
-                    <a href="#">Tiện ích</a>
+                    <a href="#">{item?.attributes?.status}</a>
                   </li>
-                )}
-                <li className="list-inline-item">
-                  <a href="#">{item?.attributes?.status}</a>
-                </li>
-              </ul>
-              <ul className="icon mb0">
-                <li className="list-inline-item">
-                  <a href="#">
-                    <span className="flaticon-transfer-1"></span>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <span className="flaticon-heart"></span>
-                  </a>
-                </li>
-              </ul>
-              <a className="fp_price">
-                {doctien(item?.attributes?.price)}
-                <small>
-                  {item?.attributes?.status === "Bán" ? "" : "/tháng"}
-                </small>
-              </a>
-            </div>
-          </div>
-          <div className="details">
-            <div className="tc_content">
-              <p className="text-thm">
-                {item?.attributes?.categoryID?.data?.attributes?.name}
-              </p>
-              <h4>
-                <Link href={`/${item?.attributes?.slug}`}>
-                  {item?.attributes?.name}
-                </Link>
-              </h4>
-              <p>
-                <span className="flaticon-placeholder"></span>
-                {item?.attributes?.location?.data?.attributes?.name}
-              </p>
-
-              <ul className="prop_details mb0">
-                <li className="list-inline-item">
-                  Phòng ngủ: {item?.attributes?.bedRoom}
-                </li>
-                <li className="list-inline-item">
-                  Phòng tắm: {item?.attributes?.bathRoom}
-                </li>
-              </ul>
-
-              <ul className="prop_details mb0">
-                {item?.attributes?.feature_ids?.data?.map((val, i) => (
-                  <li className="list-inline-item" key={i}>
-                    {val.attributes.name}
+                </ul>
+                <ul className="icon mb0">
+                  <li className="list-inline-item">
+                    <a href="#">
+                      <span className="flaticon-transfer-1"></span>
+                    </a>
                   </li>
-                ))}
-              </ul>
+                  <li className="list-inline-item">
+                    <a href="#">
+                      <span className="flaticon-heart"></span>
+                    </a>
+                  </li>
+                </ul>
+                <a className="fp_price">
+                  {doctien(item?.attributes?.price)}
+                  <small>
+                    {item?.attributes?.status === "Bán" ? "" : "/tháng"}
+                  </small>
+                </a>
+              </div>
             </div>
-            {/* End .tc_content */}
+            <div className="details">
+              <div className="tc_content">
+                <p className="text-thm">
+                  {item?.attributes?.categoryID?.data?.attributes?.name}
+                </p>
+                <h4>
+                  <Link href={`/${item?.attributes?.slug}?id=${item?.id}`}>
+                    {item?.attributes?.name}
+                  </Link>
+                </h4>
+                <p>
+                  <span className="flaticon-placeholder"></span>
+                  {item?.attributes?.location?.data?.attributes?.name}
+                </p>
 
-            {/* <div className="fp_footer">
+                <ul className="prop_details mb0">
+                  <li className="list-inline-item">
+                    Phòng ngủ: {item?.attributes?.bedRoom}
+                  </li>
+                  <li className="list-inline-item">
+                    Phòng tắm: {item?.attributes?.bathRoom}
+                  </li>
+                </ul>
+
+                <ul className="prop_details mb0">
+                  {item?.attributes?.feature_ids?.data?.map((val, i) => (
+                    <li className="list-inline-item" key={i}>
+                      {val.attributes.name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              {/* End .tc_content */}
+
+              {/* <div className="fp_footer">
               <ul className="fp_meta float-start mb0">
                 <li className="list-inline-item">
                   <Link href="/agent-v2">
@@ -132,9 +133,10 @@ const FeaturedItem = () => {
               </ul>
               <div className="fp_pdate float-end">{item.postedYear}</div>
             </div> */}
-            {/* End .fp_footer */}
+              {/* End .fp_footer */}
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
     );
   });
