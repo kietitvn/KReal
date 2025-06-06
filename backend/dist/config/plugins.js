6f673ca68bcc4dfee64f5669d745c0635711fdbb
Kiet Nguyen <kiet.itvn@gmail.com>
2025-06-06 15:16:55 +0700

    /backend redeploy ok

--- backend/dist/config/plugins.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// ./config/plugins.ts
-exports.default = () => ({
-    "media-prefix": {
-        enabled: true,
-    },
-});
